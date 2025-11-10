5ea2451a33d23d354b22370a624381a6573f8c19
Earl <earlpetaway@gmail.com>
2025-11-10 14:42:15 -0500

    Improve login response to include JWT token for easier testing

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -52,7 +52,11 @@ regd_users.post("/login", (req,res) => {
     req.session.authorization = {
       accessToken, username
     }
-    return res.status(200).send("User successfully logged in");
+    return res.status(200).json({ 
+      message: "User successfully logged in",
+      token: accessToken,
+      username: username
+    });
   } else {
     return res.status(208).json({ message: "Invalid Login. Check username and password" });
   }
